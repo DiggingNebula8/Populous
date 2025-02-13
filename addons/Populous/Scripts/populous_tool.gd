4b87c3cce4bbdf8bfca49912789e594e744955b5
Siva <v.sivavadlamani@gmail.com>
2025-02-13 17:51:04 +0530

    Added Signals to the Main and Placement settings

--- addons/Populous/Scripts/populous_tool.gd
+++ addons/Populous/Scripts/populous_tool.gd
@@ -1,6 +1,8 @@
 @tool
 extends VBoxContainer
 
+class_name PopulousTool
+
 const PopulousConstants = preload("res://addons/Populous/Scripts/populous_constants.gd")
 
 var populous_menu: VBoxContainer
@@ -8,6 +10,12 @@ var menu_disabled_label: Label
 
 var is_container_selected: bool = false
 var populous_container: Node3D = null
+
+var populous_density: int = 2
+var populous_type: int = 0
+var spawn_padding: Vector3 = Vector3(2, 0, 2)
+var rows: int = 1
+var columns: int = 2
 
 func _ready() -> void:
 	populous_menu = %PopulousMenu
@@ -21,7 +29,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	populous_menu.visible = is_container_selected
 	menu_disabled_label.visible = not is_container_selected
-
+	
 func _on_selection_changed() -> void:
 	# Get the selected nodes
 	var selected_nodes = EditorInterface.get_selection().get_selected_nodes()
@@ -43,3 +51,27 @@ func _on_generate_populous_pressed() -> void:
 	var spawned_npc = %NPCResourcePicker.edited_resource.instantiate()
 	populous_container.add_child(spawned_npc)
 	spawned_npc.owner = get_tree().edited_scene_root
+
+
+func _on_density_value_changed(value: float) -> void:
+	populous_density = int(value)
+
+
+func _on_rows_value_changed(value: float) -> void:
+	rows = int(value)
+
+
+func _on_columns_value_changed(value: float) -> void:
+	columns = int(value)
+
+
+func _on_padding_x_value_changed(value: float) -> void:
+	spawn_padding.x = int(value)
+
+
+func _on_padding_y_value_changed(value: float) -> void:
+	spawn_padding.y = int(value)
+
+
+func _on_padding_z_value_changed(value: float) -> void:
+	spawn_padding.z = int(value)
