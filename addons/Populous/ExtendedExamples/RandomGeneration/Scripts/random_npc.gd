a7ebab124363df507803b20a8cafcbaa0feb747e
Siva <v.sivavadlamani@gmail.com>
2025-02-20 09:56:59 +0530

    Added Label3D on top of RandomNPC which shows their name

--- addons/Populous/ExtendedExamples/RandomGeneration/Scripts/random_npc.gd
+++ addons/Populous/ExtendedExamples/RandomGeneration/Scripts/random_npc.gd
@@ -3,14 +3,20 @@ extends CharacterBody3D
 
 var npc: MeshInstance3D
 var mat: StandardMaterial3D
+var meta_label: Label3D
 
 func _ready() -> void:
 	npc = %MeshInstance3D
 	mat = npc.get_surface_override_material(0)
+	meta_label = %MetaLabel
+	if self.has_meta("FirstName") and self.has_meta("LastName"):
+		meta_label.text = self.get_meta("FirstName") + " " + self.get_meta("LastName")
 	if self.has_meta("Albedo"):	
 		mat.albedo_color = self.get_meta("Albedo")
 		
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
+		if self.has_meta("FirstName") and self.has_meta("LastName"):
+			meta_label.text = self.get_meta("FirstName") + " " + self.get_meta("LastName")
 		if self.has_meta("Albedo"):	
 			mat.albedo_color = self.get_meta("Albedo")
