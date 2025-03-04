f080d63d49a96de709ef57a8d9fb53a2b0a69724
Siva <v.sivavadlamani@gmail.com>
2025-03-04 18:36:37 +0530

    Gendered CapsuleCity Characters

--- addons/Populous/ExtendedExamples/MixnMatchGenerator/Scripts/capsule_city_parts.gd
+++ addons/Populous/ExtendedExamples/MixnMatchGenerator/Scripts/capsule_city_parts.gd
@@ -1,15 +1,52 @@
 @tool
 class_name CapsuleCityParts extends Resource
 
-@export var hair: Array[PackedScene]
-@export var head: Array[PackedScene]
-@export var head_prop: Array[PackedScene]
-@export var eyes: Array[PackedScene]
-@export var mouth: Array[PackedScene]
-@export var torso: Array[PackedScene]
-@export var arms: Array[PackedScene]
-@export var arm_sleeve: Array[PackedScene]
-@export var body_prop: Array[PackedScene]
-@export var legs: Array[PackedScene]
+@export_group("Hair")
+@export var hair_female: Array[PackedScene]
+@export var hair_male: Array[PackedScene]
+@export var hair_neutral: Array[PackedScene]
 
-	
+@export_group("Head")
+@export var head_female: Array[PackedScene]
+@export var head_male: Array[PackedScene]
+@export var head_neutral: Array[PackedScene]
+
+@export_group("Head Prop")
+@export var head_prop_female: Array[PackedScene]
+@export var head_prop_male: Array[PackedScene]
+@export var head_prop_neutral: Array[PackedScene]
+
+@export_group("Eyes")
+@export var eyes_female: Array[PackedScene]
+@export var eyes_male: Array[PackedScene]
+@export var eyes_neutral: Array[PackedScene]
+
+@export_group("Mouth")
+@export var mouth_female: Array[PackedScene]
+@export var mouth_male: Array[PackedScene]
+@export var mouth_neutral: Array[PackedScene]
+
+@export_group("Torso")
+@export var torso_female: Array[PackedScene]
+@export var torso_male: Array[PackedScene]
+@export var torso_neutral: Array[PackedScene]
+
+@export_group("Arms")
+@export var arms_female: Array[PackedScene]
+@export var arms_male: Array[PackedScene]
+@export var arms_neutral: Array[PackedScene]
+
+@export_group("Arm Sleeve")
+@export var arm_sleeve_female: Array[PackedScene]
+@export var arm_sleeve_male: Array[PackedScene]
+@export var arm_sleeve_neutral: Array[PackedScene]
+
+@export_group("Body Prop")
+@export var body_prop_female: Array[PackedScene]
+@export var body_prop_male: Array[PackedScene]
+@export var body_prop_neutral: Array[PackedScene]
+
+@export_group("Legs")
+@export var legs_female: Array[PackedScene]
+@export var legs_male: Array[PackedScene]
+@export var legs_neutral: Array[PackedScene]
--- addons/Populous/ExtendedExamples/MixnMatchGenerator/mix_match_populous_meta.gd
+++ addons/Populous/ExtendedExamples/MixnMatchGenerator/mix_match_populous_meta.gd
@@ -3,10 +3,38 @@ class_name MixMatchPopulousMeta extends PopulousMeta
 
 var modular_pieces: CapsuleCityParts = preload("res://addons/Populous/ExtendedExamples/MixnMatchGenerator/Resources/CapsuleCityParts.tres")
 var material: ORMMaterial3D = preload("res://addons/Populous/ExtendedExamples/MixnMatchGenerator/Assets/AssetHunts-CapsuleCityPeople/CapsuleCity.tres")
+
+@export var names_list: JSONResource = preload("res://addons/Populous/ExtendedExamples/RandomGeneration/Resources/MetaResource/RandomNames.tres")
+
+enum Gender { MALE, FEMALE }
+
+const first_name_key: StringName = "FirstName"
+const last_name_key: StringName = "LastName"
+
+var first_name: String
+var last_name: String
+
+func generate_first_name(gender: Gender) -> String:
+	var names
+	if gender == Gender.FEMALE:
+		names = names_list.data.FemaleFirstNames
+	elif gender == Gender.MALE:
+		names = names_list.data.MaleFirstNames
+		
+	return names[randi() % names.size()]
+
+func generate_last_name() -> String:
+	var names = names_list.data.LastNames
+	return names[randi() % names.size()]
+
 func set_metadata(npc: Node) -> void:
-	npc.name = "PopulousNPC"
-	npc.set_meta("PopulousMeta",true)
-	apply_modular_pieces(npc)
+	var gender = Gender.FEMALE if randi_range(0,1) else Gender.MALE
+	first_name = generate_first_name(gender)
+	last_name = generate_last_name()
+	npc.name = first_name + "-" + last_name
+	npc.set_meta(first_name_key, first_name)
+	npc.set_meta(last_name_key, last_name)
+	apply_modular_pieces(npc, gender)
 
 func _get_params() -> Dictionary:
 	return {}  # Can be extended in child classes
@@ -15,7 +43,7 @@ func _set_params(params: Dictionary) -> void:
 	pass  # Child classes handle actual params
 	
 
-func apply_modular_pieces(npc: Node) -> void:
+func apply_modular_pieces(npc: Node, gender: Gender) -> void:
 	if modular_pieces == null:
 		print_debug("[ERROR] No modular pieces assigned to CapsuleCityPerson!")
 		return
@@ -27,16 +55,35 @@ func apply_modular_pieces(npc: Node) -> void:
 		mesh_container.name = "mesh_container"
 		npc.add_child(mesh_container)
 		mesh_container.owner = npc.get_tree().edited_scene_root
-
-	# Iterate over actual properties
+		
+	var genderString: String
+	if gender == Gender.FEMALE:
+		genderString = "female"
+	elif gender == Gender.MALE:
+		genderString = "male"
+		
+	# Iterate over modular parts
 	for part_name in ["hair", "head", "head_prop", "eyes", "mouth", "torso", "arms", "arm_sleeve", "body_prop", "legs"]:
-		var piece_array: Array[PackedScene] = modular_pieces.get(part_name)
+		var piece_array: Array[PackedScene] = []
 
-		if piece_array and not piece_array.is_empty():
-			print("Spawning: ", part_name)  # Debugging
+		# Add gender-specific parts first
+		var gender_key = part_name + "_" + genderString
+		var gender_pieces = modular_pieces.get(gender_key)
+		if gender_pieces:
+			piece_array.append_array(gender_pieces)
+
+		# Always include neutral parts
+		var neutral_key = part_name + "_neutral"
+		var neutral_pieces = modular_pieces.get(neutral_key)
+		if neutral_pieces:
+			piece_array.append_array(neutral_pieces)
+
+		# Spawn part if available
+		if not piece_array.is_empty():
+			print("Spawning:", part_name, "from options:", piece_array.size())  # Debugging
 			var instance: Node3D = piece_array.pick_random().instantiate()
-			instance.name = part_name  # Name it properly
-			mesh_container.add_child(instance)  # Attach it to the NPC
+			instance.name = part_name  # Keep a consistent name
+			mesh_container.add_child(instance)
 			instance.owner = mesh_container.get_tree().edited_scene_root
 
 			# Apply material recursively
