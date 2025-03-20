b7da85f8ec7210de735f57f2ea26e2c6ef3d7a1a
Siva <v.sivavadlamani@gmail.com>
2025-03-21 01:42:10 +0530

    Removed Part from the constants

--- addons/Populous/ExtendedExamples/CapsulePersonGenerator/Scripts/capsule_person_constants.gd
+++ addons/Populous/ExtendedExamples/CapsulePersonGenerator/Scripts/capsule_person_constants.gd
@@ -1,7 +1,7 @@
 @tool
 class_name CapsulePersonConstants
 
-enum Part { NONE, HAIR, HEAD, EYES, MOUTH, TORSO, ARMS, SLEEVES, BODYPROP, LEGS}
+enum Part { NONE, HAIR, HEAD, HEADPROP, EYES, MOUTH, TORSO, ARMS, SLEEVES, BODYPROP, LEGS}
 enum Gender { MALE, FEMALE, NEUTRAL }
 enum SkinType { DEFAULT, LIGHT, MEDIUM, DARK }
 	
