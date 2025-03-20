84f25980cfc3e49535be5258c4bb80780d85a744
Siva <v.sivavadlamani@gmail.com>
2025-03-21 01:17:00 +0530

    Constants for Capsule Person Example

---
+++ addons/Populous/ExtendedExamples/CapsulePersonGenerator/Scripts/capsule_person_constants.gd
@@ -0,0 +1,7 @@
+@tool
+class_name CapsulePersonConstants
+
+enum Part { NONE, HAIR, HEAD, EYES, MOUTH, TORSO, ARMS, SLEEVES, BODYPROP, LEGS}
+enum Gender { MALE, FEMALE, NEUTRAL }
+enum SkinType { DEFAULT, LIGHT, MEDIUM, DARK }
+	
